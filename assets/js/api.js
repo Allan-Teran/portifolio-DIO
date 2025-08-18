894b3c70c9f06a5a8aa21989dcb2f4f4ed9b6221
Allan <allteran655@gmail.com>
2025-08-18 10:06:13 -0300

    Colocando o link correto do Json no API.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/RenanJPaula/js-developer-portfolio/refs/heads/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Allan-Teran/portifolio-DIO/refs/heads/master/assets/data/profile.json';
     const fetching = await fetch(url);
     return await fetching.json();
 }
